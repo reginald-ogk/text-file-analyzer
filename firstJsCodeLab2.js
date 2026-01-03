4e48e2511f810ec35b0a163dc04559f54364235d
Reginald <reginaldgyasi62@gmail.com>
2026-01-03 20:37:33 +0000

    Day 1 ~ lab[a sentence maker]

---
+++ firstJsCodeLab2.js
@@ -0,0 +1,22 @@
+let adjective = "calm";
+let noun = "Gen 116";
+let verb = "thinking"
+let place = "mind"
+let adjective2 = "calmer"
+let noun2 = "O"
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory) 
+
+
+adjective = "calmer";
+noun = "Gen 116 pro";
+verb = "reasoning"
+place = "mind"
+adjective2 = "calmer"
+noun2 = "0.1"
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory)
