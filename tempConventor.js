4fe1e9eee07873fb76109a2819ab7c55d88bac38
Reginald <reginaldgyasi62@gmail.com>
2026-01-07 15:14:26 +0000

    add Celsius to Fahrenheit conversion function

---
+++ tempConventor.js
@@ -0,0 +1,9 @@
+// Converts Celsius to Fahrenheit
+// Usage: convertCtoF(0) returns 32
+// Usage: convertCtoF(100) returns 212
+
+function convertCtoF(temperature) {
+  return temperature * (9/5) + 32
+}
+
+// formula degrees F = degrees C × 9/5 + 32
