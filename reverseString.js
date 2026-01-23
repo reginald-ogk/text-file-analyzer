3a5e7a11acbf1f953757ff73e0baf096723e7601
Reginald <reginaldgyasi62@gmail.com>
2026-01-23 07:37:58 +0000

    add function to reverse a given string

---
+++ reverseString.js
@@ -0,0 +1,13 @@
+/**
+ * Reverses the given string.
+ */
+
+function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
